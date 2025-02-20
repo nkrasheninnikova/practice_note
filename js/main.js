5ea7c334d55377c81ee7d76a8fbb4da7e96da5bb
Natasha Krasheninnikova <nk110905@mail.ru>
2025-02-21 00:27:42 +0700

    Vue component commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        columns: [
+            { id: 1, cards: [] },
+            { id: 2, cards: [] },
+            { id: 3, cards: [] }
+        ]
+    }
+});
