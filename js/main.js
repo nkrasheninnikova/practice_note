8b568b83d9fcce972bef459f27344771359390f3
Natasha Krasheninnikova <nk110905@mail.ru>
2025-02-21 01:48:10 +0700

    moving commit

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ Vue.component('card', {
       <h3>{{ card.title }}</h3>
       <ul>
         <li v-for="(item, index) in card.items" :key="index">
-          <input type="checkbox" v-model="item.completed">
+          <input type="checkbox" v-model="item.completed" @change="$emit('update')">
           <span :class="{ completed: item.completed }">{{ item.text }}</span>
         </li>
       </ul>
+      <p v-if="card.completedAt">Завершено: {{ card.completedAt }}</p>
     </div>
   `
 });
@@ -25,11 +26,41 @@ new Vue({
         newCardTitle: '',
         newCardItems: ['', '', '']
     },
+    created() {
+        const savedData = localStorage.getItem('noteAppData');
+        if (savedData) {
+            this.columns = JSON.parse(savedData);
+        }
+    },
     methods: {
         openForm(columnId) {
             this.formColumnId = columnId;
             this.newCardTitle = '';
             this.newCardItems = ['', '', ''];
+        },
+        closeForm() {
+            this.formColumnId = null;
+        },
+        addItem() {
+            this.newCardItems.push('');
+        },
+        removeItem(index) {
+            this.newCardItems.splice(index, 1);
+        },
+        submitForm() {
+            const column = this.columns.find(col => col.id === this.formColumnId);
+            if (this.newCardTitle && this.newCardItems.every(item => item.trim())) {
+                column.cards.push({
+                    id: Date.now(),
+                    title: this.newCardTitle,
+                    items: this.newCardItems.map(text => ({ text, completed: false })),
+                    completedAt: null
+                });
+                this.saveData();
+                this.closeForm();
+            } else {
+                alert('Заполните все поля!');
+            }
         },
         updateCard() {
             this.columns.forEach(column => {
@@ -48,32 +79,17 @@ new Vue({
             });
             this.saveData();
         },
-        saveData() {
-            localStorage.setItem('noteAppData', JSON.stringify(this.columns));
-        },
-        submitForm() {
-            const column = this.columns.find(col => col.id === this.formColumnId);
-            if (this.newCardTitle && this.newCardItems.every(item => item.trim())) {
-                column.cards.push({
-                    id: Date.now(),
-                    title: this.newCardTitle,
-                    items: this.newCardItems.map(text => ({ text, completed: false })),
-                    completedAt: null
-                });
-                this.saveData();
-                this.closeForm();
-            } else {
-                alert('Заполните все поля!');
+        moveCard(card, fromColumnId, toColumnId) {
+            const fromColumn = this.columns.find(col => col.id === fromColumnId);
+            const toColumn = this.columns.find(col => col.id === toColumnId);
+            const cardIndex = fromColumn.cards.indexOf(card);
+            if (toColumn.cards.length < (toColumnId === 2 ? 5 : Infinity)) {
+                toColumn.cards.push(card);
+                fromColumn.cards.splice(cardIndex, 1);
             }
         },
-        closeForm() {
-            this.formColumnId = null;
-        },
-        addItem() {
-            this.newCardItems.push('');
-        },
-        removeItem(index) {
-            this.newCardItems.splice(index, 1);
+        saveData() {
+            localStorage.setItem('noteAppData', JSON.stringify(this.columns));
         },
         isColumnBlocked(columnId) {
             if (columnId === 1) {
